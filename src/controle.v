288a5ddd1f26c2fe90d10d690b57c18d3683bc77
Natalia <73670894+natfb@users.noreply.github.com>
2024-05-28 20:17:52 -0300

    feito na aula

---
+++ src/controle.v
@@ -0,0 +1,22 @@
+module controle(
+	CLOCK, 
+	enter,
+	reset,
+	end_FPGA,
+	end_User,
+	end_time,
+	win,
+	match,
+	R1,
+	R2,
+	E1,
+	E2,
+	E3,
+	E4,
+	SEL
+);
+
+input wire CLOCK, enter, reset, end_FPGA, end_User, end_time, win, match;
+output wire R1, R2, E1, E2, E3, E4, SEL;
+ 
+endmodule
